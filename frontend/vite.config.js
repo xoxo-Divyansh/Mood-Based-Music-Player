4f188319d90e2c18123e3d4971bc3d227e29d437
Divyansh Raj Tripathi <drt.vip777@gmail.com>
2025-12-06 23:44:12 +0530

    Remove external dependency configuration for react-router-dom in Vite build settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    build: {
-    rollupOptions: {
-      external: ['react-router-dom']
-    }
-  }
+  base: './', // important for Render static hosting
 });
