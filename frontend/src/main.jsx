61bfbd4f566f18a89c4be371c1a1859fdb9cb266
Divyansh Raj Tripathi <drt.vip777@gmail.com>
2025-12-06 23:25:47 +0530

    Add react-router-dom for routing and update Vite configuration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter as Router } from 'react-router-dom'  // ✅ add this
+import { BrowserRouter, HashRouter as Router } from 'react-router-dom'  // ✅ add this
 
 createRoot(document.getElementById('root')).render(
     <Router>
